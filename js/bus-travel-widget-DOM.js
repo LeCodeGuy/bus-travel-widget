8200e6b5d5d312003804d377d029ae715277d982
LeCodeGuy <devanj89@gmail.com>
2023-05-31 14:00:47 +0200

    DOM code updated

--- js/bus-travel-widget-DOM.js
+++ js/bus-travel-widget-DOM.js
@@ -9,15 +9,50 @@ let singleTripCostElem = document.querySelector("#tripCostSingle");
 let returnTripNumberElem = document.querySelector("#tripNumberReturn");
 let returnTripCostElem = document.querySelector("#tripCostReturn");
 
+
+/*
+Destinations    |   Cost    |   Peak Hour increase
+Khayelitsha     |   R40     |   +25%
+Dunoon          |   R25     |   +25%
+Mitchells Plain |   R30     |   +25%
+*/
+
 // Add event listener to 'calculate' button
 btnCalculateElem.addEventListener("click",btnCalculate_onClick);
-
-// Check if values are available in localStorage
 
 // Instantiate the factory function
 const travel = travelApp();
 
 // DOM events
 function btnCalculate_onClick() {
-    console.log("Hello World!");
+    var pointsAvailable = pointsElem.value;
+    var destination = destinationElem.options[destinationElem.selectedIndex].text;
+    var travelTime = "";
+
+    // resets the values
+    singleTripNumberElem.value = 0;
+    singleTripCostElem.value = 0;
+    returnTripNumberElem.value = 0;
+    returnTripCostElem.value = 0;
+
+    // Loop through the radio buttons
+    for (var i = 0; i < travelTimeElem.length; i++) {
+        if (travelTimeElem[i].checked) {
+            // Get the value of the selected radio button
+            travelTime = travelTimeElem[i].value;
+           
+            break; // Exit the loop once the selected radio button is found
+        }
+    }
+    //check whether the traveller opted for a return trip
+    if(returnTripElem.checked ===  true && Number(pointsAvailable) > 0) {        
+
+        returnTripNumberElem.value = (Number(pointsAvailable)/Number(travel.tripCost(destination,travelTime)*2)).toFixed(2);//++;//;pointsAvailable//;
+        returnTripCostElem.value = (Number(travel.tripCost(destination,travelTime)*2)).toFixed(2); 
+    }
+    else if(returnTripElem.checked ===  false && Number(pointsAvailable) > 0){
+        singleTripNumberElem.value =(Number(pointsAvailable)/Number(travel.tripCost(destination,travelTime))).toFixed(2);// parseInt(returnTripNumberElem.value) + 1;
+        singleTripCostElem.value = (Number(travel.tripCost(destination,travelTime))).toFixed(2);
+    }
+
 }
--- js/bus-travel-widget.js
+++ js/bus-travel-widget.js
@@ -1,12 +1,50 @@
 function travelApp() {
     // Variables to be used in Factory Function
+    var cost = 0;
+    function calculate(destination,travelTime) {
 
-    function travelTime() {
-
+    }
+    function tripCost(destination,travelTime){
+        switch(destination){
+            case "Khayelitsha":
+                if(travelTime === "Peak"){
+                    cost = 40 * 1.25;
+                }
+                else{
+                    cost = 40;
+                }
+    
+                // if(returnTrip === true){
+                //     cost = Number(cost) * 2;
+                // }
+    
+                
+                break;
+            case "Dunoon":
+                if(travelTime === "Peak"){
+                    cost = 25 * 1.25;
+                }
+                else{
+                    cost = 25;
+                }
+                break;
+            case "Mitchells Plain":
+                if(travelTime === "Peak"){
+                    cost = 30 * 1.25;
+                }
+                else{
+                    cost = 30;
+                }
+                break;
+        }
+        return cost;
+    }
+    function travelTime(optionSelected) {
+        //if
     }
 
     function tripType() {
-
+        
     }
 
     function singleTrips() {
@@ -26,6 +64,8 @@ function travelApp() {
     }
 
     return {
+        calculate,
+        tripCost,
         travelTime,
         tripType,
         singleTrips,
